b68e9048cfc38f9738e01e89654ec05cd0d76588
Janknn <jerem.pelchat@gmail.com>
2023-06-21 01:31:12 +0200

    :sparkles:display forecast 5 days

--- Rattrapages-Pelchat-Jeremy/API/script.js
+++ Rattrapages-Pelchat-Jeremy/API/script.js
@@ -1,7 +1,22 @@
 const API_KEY = 'fab4e7a4d7d8607cbd87aca04a4900be';
 
-function getWeatherData() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=${API_KEY}`;
+function getCurrentWeatherData() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&lang=fr&appid=${API_KEY}`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur lors de la requête : ${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Une erreur s\'est produite lors de la récupération des données météorologiques actuelles :', error.message);
+    });
+}
+
+function getForecastWeatherData() {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=Paris&units=metric&lang=fr&appid=${API_KEY}`;
 
   return fetch(url)
     .then(response => {
@@ -17,18 +32,40 @@ function getWeatherData() {
 
 const weatherInfoElement = document.getElementById('weather-info');
 
-function displayWeatherInfo(weatherData) {
-  const temperature = weatherData.main.temp;
-  const weatherDescription = weatherData.weather[0].description;
-  
-  const weatherString = `Température : ${temperature}°C<br>Description : ${weatherDescription}`;
+function displayWeatherInfo(currentWeatherData, forecastWeatherData) {
+  let forecastString = '';
 
-  weatherInfoElement.innerHTML = weatherString;
+  const currentTemperature = currentWeatherData.main.temp;
+  const currentWeatherDescription = currentWeatherData.weather[0].description;
+  forecastString += `Temps actuel - Température : ${currentTemperature}°C, Description : ${currentWeatherDescription}<br><br>`;
+
+  let forecastCount = 0;
+  for (let i = 0; i < forecastWeatherData.list.length; i++) {
+    const forecastDayWeather = forecastWeatherData.list[i];
+    const forecastDateTime = new Date(forecastDayWeather.dt_txt);
+    const forecastHour = forecastDateTime.getHours();
+
+    if (forecastHour === 12 || forecastHour === 18) {
+      forecastCount++;
+      if (forecastCount > 10) {
+        break;
+      }
+
+      const forecastTemperature = forecastDayWeather.main.temp;
+      const forecastWeatherDescription = forecastDayWeather.weather[0].description;
+      const forecastDateString = forecastDateTime.toLocaleDateString('fr-FR', { weekday: 'long' });
+      const forecastTimeString = forecastDateTime.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+
+      forecastString += `${forecastDateString} à ${forecastTimeString} - Température : ${forecastTemperature}°C, Description : ${forecastWeatherDescription}<br>`;
+    }
+  }
+
+  weatherInfoElement.innerHTML = forecastString;
 }
 
-getWeatherData()
-  .then(weatherData => {
-    displayWeatherInfo(weatherData);
+Promise.all([getCurrentWeatherData(), getForecastWeatherData()])
+  .then(([currentWeatherData, forecastWeatherData]) => {
+    displayWeatherInfo(currentWeatherData, forecastWeatherData);
   })
   .catch(error => {
     console.error('Une erreur s\'est produite lors de la récupération des données météorologiques :', error.message);
