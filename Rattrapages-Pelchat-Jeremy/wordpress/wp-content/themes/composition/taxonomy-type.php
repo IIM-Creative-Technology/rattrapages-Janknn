1963e856a21f34b701f40c71a3240b1216ef0ac0
Janknn <jerem.pelchat@gmail.com>
2023-06-23 01:19:17 +0200

    add taxonomy

---
+++ Rattrapages-Pelchat-Jeremy/wordpress/wp-content/themes/composition/taxonomy-type.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Composition
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+ 
+		<?php if (have_posts()) : ?>
+ 
+			<header class="page-header">
+				<h1 class="page-title"><?php single_term_title(); ?></h1>
+			</header>
+ 
+			<?php while (have_posts()) : the_post(); ?>
+ 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					</header>
+ 
+					<div class="entry-content">
+						<?php the_excerpt(); ?>
+					</div>
+				</article>
+ 
+			<?php endwhile; ?>
+ 
+			<?php the_posts_navigation(); ?>
+ 
+		<?php else : ?>
+ 
+			<p>Aucun ingrédient trouvé.</p>
+ 
+		<?php endif; ?>
+ 
+	</main>
+</div>
+ 
+ <?php get_footer(); ?>
+ 
