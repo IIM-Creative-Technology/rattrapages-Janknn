bd716ceed2c3e9bbfe198d9b4efecb55ed3e4e4a
Janknn <jerem.pelchat@gmail.com>
2023-06-25 21:37:42 +0200

    :lipstick: add color status + style

--- Rattrapages-Pelchat-Jeremy/poo/command_page.php
+++ Rattrapages-Pelchat-Jeremy/poo/command_page.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="command_page.css">
     <title>Document</title>
 </head>
 
@@ -26,13 +27,46 @@
     }
 ?>
 
+<?php
+$statusClass = '';
+
+switch ($command['state']) {
+    case 'En cours':
+        $statusClass = 'blue';
+        break;
+    case 'Réalisée':
+        $statusClass = 'green';
+        break;
+    case 'Annulée':
+        $statusClass = 'red';
+        break;
+}
+?>
+
 <body>
-    <?php foreach ($commands as $command):?>
-        <h1><?php echo $command['first_name'];?></h1>
-        <h1><?php echo $command['last_name'];?></h1>
-        <h1><?php echo $command['address'];?></h1>
-        <h1><?php echo $command['price'];?></h1>
-        <h1><?php echo $command['state'];?></h1>
+    <?php foreach ($commands as $command): ?>
+        <h1><?php echo $command['first_name']; ?></h1>
+        <h1><?php echo $command['last_name']; ?></h1>
+        <h1><?php echo $command['address']; ?></h1>
+        <h1><?php echo $command['price']; ?></h1>
+
+        <?php
+        $statusClass = '';
+
+        switch ($command['state']) {
+            case 'En cours':
+                $statusClass = 'blue';
+                break;
+            case 'Réalisée':
+                $statusClass = 'green';
+                break;
+            case 'Annulée':
+                $statusClass = 'red';
+                break;
+        }
+        ?>
+
+        <h1 style="color: <?php echo $statusClass; ?>"><?php echo $command['state']; ?></h1>
 
         <form method="post" action="">
             <input type="hidden" name="command_id" value="<?php echo $command['id']; ?>">
@@ -43,7 +77,6 @@
             </select>
             <button type="submit">Mettre à jour le statut</button>
         </form>
-
     <?php endforeach; ?>
 </body>
 </html>
--- Rattrapages-Pelchat-Jeremy/poo/index.php
+++ Rattrapages-Pelchat-Jeremy/poo/index.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
     <form method='POST'>
+        <h1>Gestion de commande</h1>
+        <label for="first_name">Prémon</label>
         <input type="text" name="first_name">
+        <label for="last_name">Nom</label>
         <input type="text" name="last_name">
+        <label for="address">Adresse</label>
         <input type="text" name="address">
         <input type="submit" name="send" value="commander">
     </form>
